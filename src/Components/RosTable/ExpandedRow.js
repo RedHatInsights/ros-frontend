612bdf3a018809ca3de5956092d3672d232bd303
kgaikwad <kavitagaikwad103@gmail.com>
2020-12-04 15:51:23 +0530

    refactor description list in expanded view

--- src/Components/RosTable/ExpandedRow.js
+++ src/Components/RosTable/ExpandedRow.js
@@ -2,47 +2,36 @@ import React from 'react';
 import { Button } from '@patternfly/react-core';
 import propTypes from 'prop-types';
 
-export const ExpandedRow = ({ id, provider, instanceType, idlingType, ioWait }) =>  (
-    <dl className='pf-c-description-list pf-m-horizontal'>
-        <div id={id} className='pf-c-description-list__group'>
+export const ExpandedRow = ({ id, provider, instanceType, idlingType, ioWait }) =>  {
+    const inlineFieldsArray = [
+        { label: 'Provider', value: provider },
+        { label: 'Instance Type', value: instanceType },
+        { label: 'Idling type', value: idlingType },
+        { label: 'I/O Wait', value: ioWait }
+    ];
+
+    const inlineFieldsContent = inlineFieldsArray.map((field, index) =>
+        <div key={index} className='pf-c-description-list__group'>
             <dt className='pf-c-description-list__term'>
-                <span className='pf-c-description-list__text'>Provider:</span>
+                <span className='pf-c-description-list__text'>{ field.label }:</span>
             </dt>
             <dd className='pf-c-description-list__description'>
-                <div className='pf-c-description-list__text'>{ provider }</div>
+                <div className='pf-c-description-list__text'>{  field.value }</div>
             </dd>
         </div>
-        <div className='pf-c-description-list__group'>
-            <dt className='pf-c-description-list__term'>
-                <span className='pf-c-description-list__text'>Instance Type:</span>
-            </dt>
-            <dd className='pf-c-description-list__description'>
-                <div className='pf-c-description-list__text'>{ instanceType }</div>
-            </dd>
-        </div>
-        <div className='pf-c-description-list__group'>
-            <dt className='pf-c-description-list__term'>
-                <span className='pf-c-description-list__text'>Idling type:</span>
-            </dt>
-            <dd className='pf-c-description-list__description'>
-                <div className='pf-c-description-list__text'>{ idlingType }</div>
-            </dd>
-        </div>
-        <div className='pf-c-description-list__group'>
-            <dt className='pf-c-description-list__term'>
-                <span className='pf-c-description-list__text'>I/O Wait</span>
-            </dt>
-            <dd className='pf-c-description-list__description'>
-                <div className='pf-c-description-list__text'>{ ioWait }</div>
-            </dd>
-        </div>
-        <div className='pf-c-description-list__group'>
-            <dt className='pf-c-description-list__term'>
-                <Button variant="primary">X Recommendations</Button>
-            </dt>
-        </div>
-    </dl>
-);
+    );
+
+    return (
+        <dl  id={id} className='pf-c-description-list pf-m-horizontal'>
+            { inlineFieldsContent }
+            <div className='pf-c-description-list__group'>
+                <dt className='pf-c-description-list__term'>
+                    <Button variant="primary">X Recommendations</Button>
+                </dt>
+            </div>
+        </dl>
+    );
+};
 
 ExpandedRow.propTypes = {
     id: propTypes.string,
