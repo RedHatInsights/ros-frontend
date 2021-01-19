56f21380c1b3a777fd7eb2eabc88a04c6731c45d
kgaikwad <kavitagaikwad103@gmail.com>
2021-01-19 14:43:05 +0530

    add custom_release to push to ci envs

---
+++ .travis/custom_release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
+    for env in ci
+    do
+        echo "PUSHING ${env}-beta & ${env}-stable"
+        rm -rf ./dist/.git
+        .travis/release.sh "${env}-beta"
+        .travis/release.sh "${env}-stable"
+    done
+fi
