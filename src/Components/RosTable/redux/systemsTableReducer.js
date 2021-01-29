397fe7f67a9d3da57fe4b91ee3ddf7cd8326c1ae
kgaikwad <kavitagaikwad103@gmail.com>
2021-01-29 14:39:32 +0530

    remove console log statement

--- src/Components/RosTable/redux/systemsTableReducer.js
+++ src/Components/RosTable/redux/systemsTableReducer.js
@@ -9,7 +9,6 @@ const initialState = {
 const systemsTableReducer = applyReducerHash({
     FETCH_CLOUD_SYSTEMS_LIST_PENDING: (state) => ({ ...state, loading: true }),
     FETCH_CLOUD_SYSTEMS_LIST_FULFILLED: (state, action) => {
-        console.log(action);
         return { ...state, loading: false, systemsData: action.payload };
     },
     FETCH_CLOUD_SYSTEMS_LIST_REJECTED: (state, action) => ({
