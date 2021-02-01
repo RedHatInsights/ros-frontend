011bd0684f53a86fd722d341af4f69155dfe89c3
kgaikwad <kavitagaikwad103@gmail.com>
2021-02-01 20:28:06 +0530

    Fixes #49 - add deployment config for qa

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,11 +4,20 @@ set -x
 
 if [ "${TRAVIS_BRANCH}" = "main" ]
 then
-    for env in ci
+    for env in ci qa
     do
-        echo "PUSHING ${env}-beta & ${env}-stable"
+        echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
+    done
+fi
+
+
+if [ "${TRAVIS_BRANCH}" = "stable" ]
+then
+    for env in ci qa
+    do
+        echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
