d727a983c954118cb4cf90e517d6d7bc18ec59f4
kgaikwad <kavitagaikwad103@gmail.com>
2021-02-01 16:21:49 +0530

    use limit & offset as query params for pagination

--- src/Components/RosTable/redux/actions.js
+++ src/Components/RosTable/redux/actions.js
@@ -1,6 +1,10 @@
 import { ROS_API_ROOT, SYSTEMS_API_ROOT } from '../../../constants';
 
-function fetchSystems(params = {}) {
+function fetchSystems(fetchParams = {}) {
+    let params = {};
+    params.limit = fetchParams.perPage;
+    params.offset = (fetchParams.page - 1) * fetchParams.perPage;
+
     let url = new URL(ROS_API_ROOT + SYSTEMS_API_ROOT,  window.location.origin);
     url.search = new URLSearchParams(params).toString();
     return {
