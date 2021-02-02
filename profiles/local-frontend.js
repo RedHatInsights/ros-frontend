e695fa926d621762c9f6280e3bc7fa686b259b5a
kgaikwad <kavitagaikwad103@gmail.com>
2021-02-03 00:30:01 +0530

    add good-guy-http package to set timeout (#14)

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,5 +1,7 @@
 /*global module*/
 
+const goodGuyLib = require('good-guy-http');
+
 const SECTION = 'insights';
 const APP_ID = 'ros';
 const FRONTEND_PORT = 8002;
@@ -10,4 +12,12 @@ routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_POR
 routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
 
-module.exports = { routes };
+const esi = {
+    // Increases the default (2s) timeout which can be a pain sometimes.
+    // https://github.com/Schibsted-Tech-Polska/good-guy-http/blob/master/lib/index.js#L55
+    httpClient: goodGuyLib({
+        timeout: 5000
+    })
+};
+
+module.exports = { routes, esi };
