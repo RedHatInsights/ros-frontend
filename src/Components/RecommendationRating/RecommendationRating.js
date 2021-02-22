193f753ee7b5902ad0c854bd06c26fa4e063ae9d
kgaikwad <kavitagaikwad103@gmail.com>
2021-02-22 14:46:42 +0530

    add recommendation_rating component

---
+++ src/Components/RecommendationRating/RecommendationRating.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
+import OutlinedThumbsDownIcon from '@patternfly/react-icons/dist/js/icons/outlined-thumbs-down-icon';
+import OutlinedThumbsUpIcon from '@patternfly/react-icons/dist/js/icons/outlined-thumbs-up-icon';
+import PropTypes from 'prop-types';
+import ThumbsDownIcon from '@patternfly/react-icons/dist/js/icons/thumbs-down-icon';
+import ThumbsUpIcon from '@patternfly/react-icons/dist/js/icons/thumbs-up-icon';
+
+import './RecommendationRating.scss';
+import { ROS_API_ROOT, RECOMMENDATION_RATING_API } from '../../constants';
+
+const RecommendationRating = ({ system }) => {
+    const [rating, setRating] = useState(system.rating);
+    const [submitted, setSubmitted] = useState(false);
+    const [thankYou, setThankYou] = useState('Thank you for your feedback!');
+    const updateRecommendationRating = async (newRating) => {
+        const calculatedRating = rating === newRating ? 0 : newRating;
+        let url = new URL(ROS_API_ROOT + RECOMMENDATION_RATING_API,  window.location.origin);
+        try {
+
+            // POST request using fetch with error handling
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ system: system.system_id, rating: calculatedRating })
+            };
+
+            await fetch(url, requestOptions);
+            setRating(calculatedRating);
+            setSubmitted(true);
+            setTimeout(() => setThankYou(''), 3000);
+        } catch (error) {
+            console.error(error); // eslint-disable-line no-console
+        }
+    };
+
+    return <span className='ratingSpanOverride'>
+        {'Is this recommendation helpful?'}
+        <Button variant="plain" aria-label="thumbs-up" onClick={() => updateRecommendationRating(1)}
+            ouiaId="thumbsUp">
+            {rating === 1 ? <ThumbsUpIcon className='like' size='sm' /> :
+                <OutlinedThumbsUpIcon size='sm' />}
+        </Button>
+        <Button variant="plain" aria-label="thumbs-down" onClick={() => updateRecommendationRating(-1)}
+            ouiaId="thumbsDown">
+            {rating === -1 ? <ThumbsDownIcon className='dislike' size='sm' /> :
+                <OutlinedThumbsDownIcon size='sm' />}
+        </Button>
+        {submitted && thankYou}
+    </span>;
+};
+
+RecommendationRating.propTypes = {
+    system: PropTypes.object
+};
+
+export default RecommendationRating;
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const ROS_API_ROOT = '/api/ros/v0';
 export const SYSTEMS_API_ROOT = '/systems';
+export const RECOMMENDATION_RATING_API = '/rating';
