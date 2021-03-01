b524b03160463d0badc105895997f9fa445f0906
kgaikwad <kavitagaikwad103@gmail.com>
2021-03-01 11:58:45 +0530

    replace existing keys with new response keys

--- src/Components/RosTable/ExpandedRow.js
+++ src/Components/RosTable/ExpandedRow.js
@@ -8,7 +8,7 @@ import {
     TextListItemVariants
 } from '@patternfly/react-core';
 
-export const ExpandedRow = ({ id, cloudProvider, instanceType, idlingTime, ioWait }) =>  {
+export const ExpandedRow = ({ inventoryId, cloudProvider, instanceType, idlingTime, ioWait }) =>  {
     const inlineFieldsArray = [
         { label: 'Provider', value: cloudProvider },
         { label: 'Instance Type', value: instanceType },
@@ -18,7 +18,7 @@ export const ExpandedRow = ({ id, cloudProvider, instanceType, idlingTime, ioWai
 
     return (
         <TextContent className='expanded-row'>
-            <TextList id={id} component={TextListVariants.dl}>
+            <TextList id={inventoryId} component={TextListVariants.dl}>
                 { inlineFieldsArray.map((field, index) => (
                     <React.Fragment key={index} >
                         <TextListItem component={TextListItemVariants.dt}>{ field.label }</TextListItem>
@@ -31,7 +31,7 @@ export const ExpandedRow = ({ id, cloudProvider, instanceType, idlingTime, ioWai
 };
 
 ExpandedRow.propTypes = {
-    id: propTypes.number,
+    inventoryId: propTypes.string,
     cloudProvider: propTypes.string,
     instanceType: propTypes.string,
     idlingTime: propTypes.string,
