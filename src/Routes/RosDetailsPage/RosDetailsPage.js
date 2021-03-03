d51ca219e954941cf9108ec165d1b1aeb44f9818
Aditi Puntambekar <apuntamb@redhat.com>
2021-03-03 13:30:51 +0530

    reference from edge-frontend

--- src/Routes/RosDetailsPage/RosDetailsPage.js
+++ src/Routes/RosDetailsPage/RosDetailsPage.js
@@ -1,38 +1,54 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useParams, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Title } from '@patternfly/react-core';
-import { Icon } from '@patternfly/react-icons';
-import Breadcrumbs from '../Breadcrumbs';
+import { PageHeader } from '@redhat-cloud-services/frontend-components/PageHeader';
+import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
 //import { fetchRosDetails } from '../../Components/RosTable/redux/actions';
-//import { routes as paths } from '../../../package.json';
+import { routes as paths } from '../../../package.json';
 
-const RosDetails = ({ loaded, system }) => {
-    if (!loaded) {
-        return (
-            <div>
-                <Icon name='sync-alt'/>
-            </div>
-        );
-    }
+import { InventoryDetailHead, AppInfo, DetailWrapper } from '@redhat-cloud-services/frontend-components/Inventory';
+import { useSelector } from 'react-redux';
+
+const RosDetail = () => {
+    const { inventoryId } = useParams();
+    const displayName = useSelector(
+        ({ entityDetails }) => entityDetails?.entity?.display_name
+    );
+    // do we need a Reducer similar to groupsDetailReducer for entity ?
+
+    useEffect(() => {
+        insights.chrome?.hideGlobalFilter?.(true);
+        insights.chrome.appAction('system-detail');
+    }, []);
+
+    useEffect(() => {
+        insights?.chrome?.appObjectId?.(inventoryId);
+    }, [inventoryId]);
 
     return (
-        <React.Fragment>
-            <Breadcrumbs name='Resource Optimization' path='/ros' param={system.id}/>
-            <Title size='xxl'>{system.display_name}</Title>
-            <dl>
-                <dt>hostname</dt>
-                <dd>{system.display_name}</dd>
-                <dt>system id</dt>
-                <dd>{system.id}</dd>
-            </dl>
-        </React.Fragment>
+        <DetailWrapper
+            hideInvLink
+            showTags
+            //onLoad={({ mergeWithDetail }) => {
+            //  register({
+
+            //     });
+            //}}
+        >
+            <PageHeader>
+                <Breadcrumb>
+                    <BreadcrumbItem to="#">Resource Optimization</BreadcrumbItem>
+                    <BreadcrumbItem to={paths.rosDetails}>System1</BreadcrumbItem>
+                </Breadcrumb>
+                <InventoryDetailHead fallback="" hideBack showTags hideInvDrawer />
+            </PageHeader>
+        </DetailWrapper>
     );
 };
 
-RosDetails.propTypes = {
+RosDetail.propTypes = {
     loaded: PropTypes.bool.isRequired,
     entity: PropTypes.object
 };
 
-export default withRouter(RosDetails);
+export default withRouter(RosDetail);
