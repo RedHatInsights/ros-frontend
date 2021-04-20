c4a4d4485722e03cb2dcad73bae77653cc494c82
kgaikwad <kavitagaikwad103@gmail.com>
2021-04-20 14:33:18 +0530

    RHIROS-109 - apply name filter on recommendations (#28)
    * RHIROS-109 - apply name filter on recommendations
    
    * add debounce to avoid frequent API calls
    
    * rename debounced function & change wait time to 800

--- src/Components/SystemDetail/SystemRecommendations.js
+++ src/Components/SystemDetail/SystemRecommendations.js
@@ -14,7 +14,7 @@ import {
     StackItem,
     Pagination
 } from '@patternfly/react-core';
-
+import debounce from 'lodash/debounce';
 import asyncComponent from '../../Utilities/asyncComponent';
 const RecommendationsTable = asyncComponent(() => import('./RecommendationsTable'));
 
@@ -24,6 +24,41 @@ const RecommendationsTable = asyncComponent(() => import('./RecommendationsTable
  *
  * @param props the props given by the smart component.
  */
+const defaultFilters = {
+    name: {
+        label: 'Name',
+        value: ''
+    }
+};
+
+export const isEmptyFilters = (activeFilters) => Object.values(activeFilters).find(
+    (item) => item?.value?.length > 0 || item?.length > 0
+);
+
+export const constructActiveFilters = (activeFilters) => Object.entries(activeFilters).map(([key, { label, value } = {}]) => ({
+    category: label,
+    chipKey: key,
+    chips:
+        value?.length > 0
+            ? Array.isArray(value)
+                ? value.map((item) => ({ name: item })) : [{ name: value }]
+            : []
+}));
+
+export const onDeleteFilter = (activeFilters, itemsToRemove) => {
+    const currItem = itemsToRemove[0];
+    return {
+        ...activeFilters,
+        [currItem?.chipKey]: {
+            ...(activeFilters[currItem?.chipKey] || {}),
+            value: Array.isArray(activeFilters[currItem?.chipKey]?.value)
+                ? activeFilters[currItem?.chipKey]?.value?.filter(
+                    (item) => !currItem?.chips?.find(({ name }) => name === item)
+                  )
+                : ''
+        }
+    };
+};
 
 class SystemRecommendations extends React.Component {
     constructor(props) {
@@ -31,19 +66,49 @@ class SystemRecommendations extends React.Component {
         this.state = {
             page: 1,
             perPage: 10,
-            inventoryId: props.match.params.inventoryId
+            inventoryId: props.match.params.inventoryId,
+            activeFilters: defaultFilters
         };
+        // Note that 800 is used widely accross platform
+        this.debouncedThrottleHandleChange = debounce(
+            this.throttleHandleChange.bind(this), 800
+        );
+
     }
 
     async componentDidMount() {
-        await this.props.loadSysRecs(this.state.inventoryId,
-            { page: this.state.page, perPage: this.state.perPage }
-        );
+        await this.requestLoadSysRecs();
+    }
+
+    async throttleHandleChange(options) {
+        await this.requestLoadSysRecs(options);
+    }
+
+    async requestLoadSysRecs(options = {}) {
+        const params = {
+            page: this.state.page,
+            perPage: this.state.perPage,
+            description: this.state?.descriptionFilter,
+            ...options
+        };
+        await this.props.loadSysRecs(this.state.inventoryId, params);
+    }
+
+    applyFilters(filters) {
+        this.setState({
+            activeFilters: filters,
+            descriptionFilter: filters?.name?.value,
+            page: 1
+        });
+        this.debouncedThrottleHandleChange({
+            description: filters?.name?.value,
+            page: 1
+        });
     }
 
     async updatePagination(pagination) {
         this.setState(pagination);
-        await this.props.loadSysRecs(this.state.inventoryId, pagination);
+        await this.requestLoadSysRecs(pagination);
     }
 
     render() {
@@ -65,6 +130,40 @@ class SystemRecommendations extends React.Component {
                                     isCompact: true,
                                     widgetId: 'ros-pagination-top'
                                 }}
+                                filterConfig={{
+                                    items: [{
+                                        label: defaultFilters.name.label,
+                                        type: 'text',
+                                        filterValues: {
+                                            key: 'text-filter',
+                                            onChange: (event, value) => {
+                                                const activeFilters = {
+                                                    ...this.state.activeFilters,
+                                                    name: {
+                                                        ...(this.state.activeFilters?.name || {}),
+                                                        value
+                                                    }
+                                                };
+                                                this.applyFilters(activeFilters);
+                                            },
+                                            value: this.state.activeFilters?.name?.value || '',
+                                            placeholder: 'Filter by name'
+                                        }
+                                    }]
+                                }}
+                                activeFiltersConfig={{
+                                    filters: isEmptyFilters(this.state.activeFilters)
+                                        ? constructActiveFilters(this.state.activeFilters)
+                                        : [],
+                                    onDelete: (event, itemsToRemove, isAll) => {
+                                        if (isAll) {
+                                            this.applyFilters(defaultFilters);
+                                        } else {
+                                            const filtersOnDeletion = onDeleteFilter(this.state.activeFilters, itemsToRemove);
+                                            this.applyFilters(filtersOnDeletion);
+                                        }
+                                    }
+                                }}
                                 />
                                 { (!this.props.loading) ? (<RecommendationsTable recommendations = { recsData }/>) : null }
                                 <TableToolbar>
@@ -78,7 +177,6 @@ class SystemRecommendations extends React.Component {
                                         onPerPageSelect={(_e, perPage) => this.updatePagination({ page: 1, perPage })}
                                     />
                                 </TableToolbar>
-
                             </CardBody>
                         </Card>
                     </StackItem>
