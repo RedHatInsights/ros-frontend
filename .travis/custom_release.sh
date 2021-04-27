7aa869702a0cd31ca605398cbc45351edcc0d460
kgaikwad <kavitagaikwad103@gmail.com>
2021-04-27 17:58:24 +0530

    edit release script to consider PROD branches (#33)

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -22,3 +22,9 @@ then
         .travis/release.sh "${env}-stable"
     done
 fi
+
+if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    echo "PUSHING ${TRAVIS_BRANCH}"
+    rm -rf ./build/.git
+    .travis/release.sh "${TRAVIS_BRANCH}"
+fi
