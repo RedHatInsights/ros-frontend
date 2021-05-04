20cb148e8e487caec6b92bed6e491575d1d6d36b
kgaikwad <kavitagaikwad103@gmail.com>
2021-05-04 16:29:42 +0530

    unit test for RosTable/ExpandedRow

---
+++ src/Components/RosTable/ExpandedRow.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import { ExpandedRow } from './ExpandedRow';
+import renderer from 'react-test-renderer';
+
+afterEach(cleanup);
+
+it ('matches snapshot', ()=>{
+    const propValues = {
+        inventoryId: 'fd480a4c-952b-4675-ba02-3370bd08a5ee',
+        cloudProvider: 'aws',
+        instanceType: 't2.micro',
+        idlingTime: '1.03',
+        ioWait: '0.10'
+    };
+
+    const tree = renderer.create(<ExpandedRow { ...propValues }/>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
