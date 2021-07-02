d2452196371ffeae3b22cf609921ff2d98cfd4a5
kgaikwad <kavitagaikwad103@gmail.com>
2021-07-02 12:26:11 +0530

    RHIROS-34 - apply RBAC permissions while rendering

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,12 @@
+import { LockIcon } from '@patternfly/react-icons';
+
+export const propsForAccessDenied = {
+    icon: LockIcon,
+    color: '#6a6e73',
+    title: 'You do not have access to Systems',
+    text: ['Contact your organization administrator(s) for more information.']
+};
+
 export const ROS_API_ROOT = '/api/ros/v0';
 export const SYSTEMS_API_ROOT = '/systems';
 export const RECOMMENDATION_RATING_API = '/rating';
