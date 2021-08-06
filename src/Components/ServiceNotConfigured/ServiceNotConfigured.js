c19ace2c72343ad57dfe36b4a20d5e087513e373
kgaikwad <kavitagaikwad103@gmail.com>
2021-08-06 21:14:41 +0530

    update with configuration instructions & PDF link

--- src/Components/ServiceNotConfigured/ServiceNotConfigured.js
+++ src/Components/ServiceNotConfigured/ServiceNotConfigured.js
@@ -1,19 +1,51 @@
-import { Bullseye, EmptyState, EmptyStateBody, EmptyStateIcon, Title, Text } from '@patternfly/react-core';
+import {
+    Bullseye, EmptyState, EmptyStateBody,
+    EmptyStateIcon, Title, Stack,
+    StackItem, ClipboardCopy, Button
+} from '@patternfly/react-core';
 import { WrenchIcon } from '@patternfly/react-icons';
 import React from 'react';
 import './ServiceNotConfigured.scss';
 
 export const ServiceNotConfigured = () => (
     <Bullseye>
-        <EmptyState className='empty-state-not-configured'>
+        <EmptyState className="empty-state-not-configured">
             <EmptyStateIcon icon={WrenchIcon} />
             <Title headingLevel="h5" size="lg">
-                Resource optimization isn&apos;t configured yet
+              Resource optimization isn&apos;t configured yet
             </Title>
             <EmptyStateBody>
-                To start configuration, activate this application in the Service Enabled Dashboard.
-                <Text>[Start configuration]</Text>
+                <Bullseye>
+                    <Stack hasGutter>
+                        <StackItem>To start configuration, follow these steps.</StackItem>
+                        <StackItem>
+                            1. Prerequisites: insights-client, ansible (Read FAQ if you
+                don&apos;t know how to install it).
+                        </StackItem>
+                        <StackItem>
+                            2. Download and run playbook.
+                        </StackItem>
+                        <StackItem>
+                            <ClipboardCopy>
+                                wget https://raw.githubusercontent.com/RedHatInsights/ros-backend/main/ansible-playbooks/ros_install_and_set_up.yml
+                            </ClipboardCopy>
+                        </StackItem>
+                        <StackItem>
+                            Append localhost to /etc/ansible/hosts
+                        </StackItem>
+                        <StackItem>
+                            <ClipboardCopy>ansible-playbook -c local ros_install_and_set_up.yml</ClipboardCopy>
+                        </StackItem>
+                        <StackItem>
+                            3. Wait <strong>24 hours</strong> for your upload to complete.
+                            At completion, metrics from the previous day will be provided.
+                        </StackItem>
+                    </Stack>
+                </Bullseye>
             </EmptyStateBody>
+            <Button component="a" href="https://access.redhat.com/articles/6245921" target="_blank" variant="primary">
+                Getting started documentation
+            </Button>
         </EmptyState>
     </Bullseye>
 );
