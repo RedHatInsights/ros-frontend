9246ef270199d63eb7b4703130c5ece2b9ade322
kgaikwad <kavitagaikwad103@gmail.com>
2021-08-18 16:13:32 +0530

    unit test for Components/RecommendationsTable

---
+++ src/Components/SystemDetail/RecommendationsTable.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import RecommendationsTable from './RecommendationsTable';
+import renderer from 'react-test-renderer';
+
+afterEach(cleanup);
+
+it ('matches snapshot', ()=>{
+    const propValues = {
+        recommendations: [{
+            ruleId: 'cloud_instance_ros_evaluation|CONSUMPTION_MODEL',
+            description: 'This is test rule description.',
+            reason: 'This may be caused due to inappropriate consumption model.',
+            resolution: 'Please verify this instance.',
+            condition: ''
+        }]
+    };
+
+    const component = renderer.create(<RecommendationsTable { ...propValues }/>);
+    expect(component.toJSON()).toMatchSnapshot();
+});
