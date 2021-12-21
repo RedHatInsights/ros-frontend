553cefc6b139d6d7f52cee4a74033f020745cd2e
Pau Garcia Quiles <pgarciaq@redhat.com>
2021-12-21 14:31:38 +0100

    Better message

--- src/Components/ServiceNotConfigured/ServiceNotConfigured.js
+++ src/Components/ServiceNotConfigured/ServiceNotConfigured.js
@@ -1,6 +1,7 @@
 import {
-    Bullseye, EmptyState,
-    EmptyStateIcon, Title, Button
+    Bullseye, EmptyState, EmptyStateBody,
+    EmptyStateIcon, Title, Stack,
+    StackItem, Button
 } from '@patternfly/react-core';
 import { WrenchIcon } from '@patternfly/react-icons';
 import React from 'react';
@@ -13,9 +14,27 @@ export const ServiceNotConfigured = () => (
             <Title headingLevel="h5" size="lg">
               Resource optimization isn&apos;t configured yet
             </Title>
-            // eslint-disable-next-line max-len
+            <EmptyStateBody>
+                <Bullseye>
+                    <Stack hasGutter>
+                        <StackItem>
+                            Resource Optimization requires installing and configuring Performance Co-Pilot on
+                            the client system.
+                        </StackItem>
+                        <StackItem>
+                            Check the documentation to find how to configure Resource Optimization with Ansible. An
+                            alternative method which does not require Ansible is also described.
+                        </StackItem>
+                        <StackItem>
+                            After configuring Resource Optimization, it may take up to 24 hours until suggestions
+                            are available.
+                        </StackItem>
+                    </Stack>
+                </Bullseye>
+            </EmptyStateBody>
+            { // eslint-disable-next-line max-len }
             <Button component="a" href="https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/proc-ros-install_ros-getting-started" target="_blank" variant="primary">
-                Check our getting started documentation to configure it.
+                Getting started documentation.
             </Button>
         </EmptyState>
     </Bullseye>
