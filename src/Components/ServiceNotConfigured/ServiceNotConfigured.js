3787c8b252fcf68f739f0552048499b31ad04e69
Pau Garcia Quiles <pgarciaq@redhat.com>
2021-12-21 12:08:06 +0100

    Show link to getting started docs when system is not configured

--- src/Components/ServiceNotConfigured/ServiceNotConfigured.js
+++ src/Components/ServiceNotConfigured/ServiceNotConfigured.js
@@ -14,37 +14,8 @@ export const ServiceNotConfigured = () => (
             <Title headingLevel="h5" size="lg">
               Resource optimization isn&apos;t configured yet
             </Title>
-            <EmptyStateBody>
-                <Bullseye>
-                    <Stack hasGutter>
-                        <StackItem>To start configuration, follow these steps.</StackItem>
-                        <StackItem>
-                            1. Prerequisites: insights-client, ansible (Read FAQ if you
-                don&apos;t know how to install it).
-                        </StackItem>
-                        <StackItem>
-                            2. Download and run playbook.
-                        </StackItem>
-                        <StackItem>
-                            <ClipboardCopy>
-                                curl -O https://raw.githubusercontent.com/RedHatInsights/ros-backend/main/ansible-playbooks/ros_install_and_set_up.yml
-                            </ClipboardCopy>
-                        </StackItem>
-                        <StackItem>
-                            Append localhost to /etc/ansible/hosts
-                        </StackItem>
-                        <StackItem>
-                            <ClipboardCopy>ansible-playbook -c local ros_install_and_set_up.yml</ClipboardCopy>
-                        </StackItem>
-                        <StackItem>
-                            3. Wait <strong>24 hours</strong> for your upload to complete.
-                            At completion, metrics from the previous day will be provided.
-                        </StackItem>
-                    </Stack>
-                </Bullseye>
-            </EmptyStateBody>
-            <Button component="a" href="https://access.redhat.com/articles/6245921" target="_blank" variant="primary">
-                Getting started documentation
+            <Button component="a" href="https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/proc-ros-install_ros-getting-started" target="_blank" variant="primary">
+                Check our getting started documentation to configure it.
             </Button>
         </EmptyState>
     </Bullseye>
