0563ef16de9bc7c7e7b6b0dc402dcd836c737ed9
Pau Garcia Quiles <pgarciaq@redhat.com>
2021-12-21 14:13:09 +0100

    More eslint happiness

--- src/Components/ServiceNotConfigured/ServiceNotConfigured.js
+++ src/Components/ServiceNotConfigured/ServiceNotConfigured.js
@@ -13,6 +13,7 @@ export const ServiceNotConfigured = () => (
             <Title headingLevel="h5" size="lg">
               Resource optimization isn&apos;t configured yet
             </Title>
+            // eslint-disable-next-line max-len
             <Button component="a" href="https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/proc-ros-install_ros-getting-started" target="_blank" variant="primary">
                 Check our getting started documentation to configure it.
             </Button>
