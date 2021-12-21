c2f6dbee969f7fef0e15a7bb5e93683de5187473
Preeti Wadhwani <pwadhwani2592@gmail.com>
2021-12-22 01:46:02 +0530

    fixed lint errors

--- src/Components/ServiceNotConfigured/ServiceNotConfigured.js
+++ src/Components/ServiceNotConfigured/ServiceNotConfigured.js
@@ -5,6 +5,7 @@ import {
 } from '@patternfly/react-core';
 import { WrenchIcon } from '@patternfly/react-icons';
 import React from 'react';
+import { GETTING_STARTED_URL } from '../../constants';
 import './ServiceNotConfigured.scss';
 
 export const ServiceNotConfigured = () => (
@@ -32,8 +33,11 @@ export const ServiceNotConfigured = () => (
                     </Stack>
                 </Bullseye>
             </EmptyStateBody>
-            { // eslint-disable-next-line max-len }
-            <Button component="a" href="https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/proc-ros-install_ros-getting-started" target="_blank" variant="primary">
+            <Button
+                component="a"
+                href={GETTING_STARTED_URL}
+                target="_blank"
+                variant="primary">
                 Getting started documentation.
             </Button>
         </EmptyState>
--- src/constants.js
+++ src/constants.js
@@ -12,3 +12,7 @@ export const POSITIVE_FEEDBACK = 1;
 // No Data State
 export const NO_DATA_STATE = 'Waiting for data';
 export const NO_DATA_VALUE = 'N/A';
+
+// Getting started URL
+// eslint-disable-next-line max-len
+export const GETTING_STARTED_URL = 'https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/proc-ros-install_ros-getting-started';
