7c6818cbaee3d35a6d3a82243102c380b51dccc3
Karel Hala <khala@redhat.com>
2022-01-17 12:40:39 +0100

    Disable sass prefix, AsyncComponent wraps now component with correct namespace

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,8 +1,7 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
-    rootFolder: resolve(__dirname, '../'),
-    sassPrefix: '.ros, .inventory'
+    rootFolder: resolve(__dirname, '../')
 });
 
 plugins.push(
