5d7de141b95e1bf5b482bbf4e4a2164920e8a0cf
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-03-07 13:17:17 +0530

    updated custom_release.sh for stage beta/stable env

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -9,19 +9,13 @@ then
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
-    done
-fi
 
-
-if [ "${TRAVIS_BRANCH}" = "stable" ]
-then
-    for env in ci qa
-    do
         echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
 fi
+
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     echo "PUSHING ${TRAVIS_BRANCH}"
