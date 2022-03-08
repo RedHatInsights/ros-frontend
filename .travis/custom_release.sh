a55f7fde0a6de67cf12ff1504a8956aeadd1da7a
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-03-08 17:23:06 +0530

    updated custom_release.sh for stage beta/stable env (#99)
    * updated custom_release.sh for stage beta/stable env
    
    * removed ci reference as we only need to push to stage

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,24 +4,18 @@ set -x
 
 if [ "${TRAVIS_BRANCH}" = "main" ]
 then
-    for env in ci qa
+    for env in qa
     do
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
-    done
-fi
 
-
-if [ "${TRAVIS_BRANCH}" = "stable" ]
-then
-    for env in ci qa
-    do
         echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
 fi
+
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     echo "PUSHING ${TRAVIS_BRANCH}"
