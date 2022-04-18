3d4d87b3d21766558bd9ff7082571cbb8490a028
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-04-18 21:49:41 +0530

    added dropdown,tooltip UI to historical chart component

--- src/Components/HistoricalDataChart/HistoricalDataChart.js
+++ src/Components/HistoricalDataChart/HistoricalDataChart.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from 'react';
-// import "@patternfly/react-core/dist/styles/base.css";
-import "./fonts.css";
-import { VictoryZoomContainer } from "victory-zoom-container";
+import React, { useState }  from 'react';
 import {
   Chart,
   ChartAxis,
@@ -9,22 +6,29 @@ import {
   ChartLine,
   ChartScatter,
   ChartThemeColor,
+  createContainer
 } from "@patternfly/react-charts";
+import { Dropdown, DropdownItem, DropdownToggle, Tooltip } from '@patternfly/react-core';
+import CaretDownIcon from '@patternfly/react-icons/dist/js/icons/caret-down-icon';
+import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
+import './HistoricalDataChart.scss';
 
 
 
 export const HistoricalDataChart = () => {
+
+    const [isOpen, setOpen] = useState(false);
 
     const series = [
         {
           datapoints: [
             { name: "CPU Utilization", x: "1 Apr", y: 10 },
             { name: "CPU Utilization", x: "2 Apr", y: 20 },
-            { name: "CPU Utilization", x: "3 Apr", y: 50 },
-            { name: "CPU Utilization", x: "4 Apr", y: 90 },
-            { name: "CPU Utilization", x: "5 Apr", y: 0 },
-            { name: "CPU Utilization", x: "6 Apr", y: 80 },
-            { name: "CPU Utilization", x: "Today", y: 40 }
+            { name: "CPU Utilization", x: "3 Apr", y: 30 },
+            { name: "CPU Utilization", x: "4 Apr", y: 50 },
+            { name: "CPU Utilization", x: "5 Apr", y: 40 },
+            { name: "CPU Utilization", x: "6 Apr", y: 30 },
+            { name: "CPU Utilization", x: "Today", y: 20 }
           ]
         },
         {
@@ -40,24 +44,58 @@ export const HistoricalDataChart = () => {
         }
     ];  
     
+    const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi");
+
+    const onToggle = (isOpen) => {
+        setOpen(isOpen);
+    }
+
+    const onSelect = (event, value) => {
+        setOpen(!isOpen);
+        console.log("checking:", event, value);
+    }
+
+    const dropdownItems =[
+        <DropdownItem key="action" component="button">Last 7 Days</DropdownItem>,
+        <DropdownItem key="action" component="button">Last 45 Days</DropdownItem>
+    ]
 
     return(
         <div>
+            <span>
+                <Tooltip content={<div>Scroll or pan to zoom in</div>}>
+                    <OutlinedQuestionCircleIcon size='sm' />
+                </Tooltip>
+                <Dropdown
+                    className='dateDropdown'
+                    onSelect={onSelect}
+                    toggle={
+                        <DropdownToggle 
+                        id='chart-date-toggle' 
+                        onToggle={onToggle} toggleIndicator={CaretDownIcon} >
+                        Last 7 Days
+                        </DropdownToggle>
+                    }
+                    isOpen={isOpen}
+                    dropdownItems={dropdownItems}
+                />
+            </span>
+            
             <div style={{ height: "275px" }}>
             <Chart
                 ariaDesc="System Utilization"
                 ariaTitle="System Utilization"
                 containerComponent={
-                    <VictoryZoomContainer
-                        responsive={false}
-                        zoomDimension="x"
-                        labels={({ datum }) => datum.childName.includes('line-') ? `${datum.name}: ${datum.y}` : null}  
+                    <VictoryZoomVoronoiContainer 
+                        labels={({ datum }) => datum.childName.includes('line-') ? `${datum.name}: ${datum.y}%` : null}  
+                        constrainToVisibleArea
                     />
                 }
                 legendData={[{ name: "CPU Utilization" }, { name: "Memory Utilization" }]}
                 legendOrientation="vertical"
                 legendPosition="right"
                 height={275}
+                width={756}
                 maxDomain={{ y: 100 }}
                 minDomain={{ y: 0 }}
                 padding={{
@@ -66,11 +104,12 @@ export const HistoricalDataChart = () => {
                     right: 200, // Adjusted to accommodate legend
                     top: 50
                 }}
-                themeColor={ChartThemeColor.blue}
-                width={756}
-            >
-                <ChartAxis tickValues={[20, 30, 40]} />
-                <ChartAxis dependentAxis showGrid tickValues={[20, 50, 80]} />
+                themeColor={ChartThemeColor.blue}>
+                <ChartAxis />
+                <ChartAxis 
+                    dependentAxis showGrid 
+                    tickValues={[0, 20, 40, 60, 80, 100]}
+                    tickFormat={(t) => `${t}%`} />
                 <ChartGroup>
                 {series.map((s, idx) => {
                     return (
