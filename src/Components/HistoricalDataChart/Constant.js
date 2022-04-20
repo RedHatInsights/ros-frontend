9c9bdf8254e5140ba1e493e95eeb42a3534eaaa2
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-04-20 20:32:40 +0530

    updated chart to show 45 days of data with custom data

---
+++ src/Components/HistoricalDataChart/Constant.js
@@ -0,0 +1,194 @@
+export const series = [
+    {
+      datapoints: [
+        { name: "CPU Utilization", x: new Date('2022-04-01'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-02'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-03'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-04'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-04-05'), y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-06'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-07'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-04-08'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-09'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-10'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-11'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-12'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-13'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-14'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-04-15'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-16'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-17'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-18'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-04-19'), y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-20'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-21'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-04-22'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-23'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-24'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-25'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-26'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-27'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-28'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-04-29'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-30'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-05-01'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-05-02'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-05-03'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-04'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-05-05'), y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-06'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-07'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-05-08'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-09'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-10'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-11'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-12'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-13'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-14'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-05-15'), y: 0 },
+      ]
+    },
+    {
+        datapoints: [
+          { name: "Memory Utilization", x: new Date('2022-04-01'), y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-04-02'), y: 10 },
+          { name: "Memory Utilization", x: new Date('2022-04-03'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-04'),  y: 50 },
+          { name: "Memory Utilization", x: new Date('2022-04-05'), y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-06'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-07'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-08'),  y: 50 },
+          { name: "Memory Utilization", x: new Date('2022-04-09'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-10'),  y: 60 },
+          { name: "Memory Utilization", x: new Date('2022-04-11'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-12'),  y: 55 },
+          { name: "Memory Utilization", x: new Date('2022-04-13'), y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-14'), y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-04-15'), y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-16'), y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-04-17'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-18'),  y: 60 },
+          { name: "Memory Utilization", x: new Date('2022-04-19'), y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-20'),  y: 10 },
+          { name: "Memory Utilization", x: new Date('2022-04-21'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-22'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-23'),  y: 25 },
+          { name: "Memory Utilization", x: new Date('2022-04-24'),  y: 35 },
+          { name: "Memory Utilization", x: new Date('2022-04-25'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-26'),  y: 60 },
+          { name: "Memory Utilization", x: new Date('2022-04-27'), y: 10 },
+          { name: "Memory Utilization", x: new Date('2022-04-28'), y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-29'), y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-04-30'), y: 10 },
+          { name: "Memory Utilization", x: new Date('2022-05-01'), y: 10 },
+          { name: "Memory Utilization", x: new Date('2022-05-02'), y: 0 },
+          { name: "Memory Utilization", x: new Date('2022-05-03'),  y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-05-04'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-05-05'), y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-05-06'),  y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-05-07'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-05-08'),  y: 50 },
+          { name: "Memory Utilization", x: new Date('2022-05-09'),  y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-05-10'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-05-11'),  y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-05-12'),  y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-05-13'), y: 20 },
+          { name: "Memory Utilization", x: new Date('2022-05-14'), y: 10 },
+          { name: "Memory Utilization", x: new Date('2022-05-15'), y: 25 },
+        ]
+    },
+
+];  
+
+export const series_7_days = [
+    {
+      datapoints: [
+        { name: "CPU Utilization", x: new Date('2022-04-03'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-04'), y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-04-05'), y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-06'), y: 30 },
+      ]
+    },
+    {
+        datapoints: [
+          { name: "Memory Utilization", x: new Date('2022-04-02'), y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-03'), y: 30 },
+          { name: "Memory Utilization", x: new Date('2022-04-04'), y: 50 },
+          { name: "Memory Utilization", x: new Date('2022-04-05'), y: 40 },
+          { name: "Memory Utilization", x: new Date('2022-04-06'), y: 40 },
+        ]
+    }
+];  
+
+export const series_random_days = [
+    {
+      datapoints: [
+        { name: "CPU Utilization", x: new Date('2022-04-01'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-02'), y: 10 },
+        { name: "CPU Utilization", x: new Date('2022-04-04'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-04-05'), y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-08'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-09'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-10'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-11'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-12'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-13'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-14'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-04-15'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-16'), y: 0 },
+        { name: "CPU Utilization", x: new Date('2022-04-17'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-20'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-23'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-24'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-26'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-04-27'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-29'), y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-04-30'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-05-03'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-04'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-05-05'), y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-06'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-07'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-05-08'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-11'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-12'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-14'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-05-15'), y: 0 },
+      ]
+    },
+];  
+
+export const series_less_days_45 = [
+    {
+      datapoints: [
+        { name: "CPU Utilization", x: new Date('2022-05-06'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-07'),  y: 50 },
+        { name: "CPU Utilization", x: new Date('2022-05-08'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-11'),  y: 30 },
+        { name: "CPU Utilization", x: new Date('2022-05-12'),  y: 40 },
+        { name: "CPU Utilization", x: new Date('2022-05-14'), y: 20 },
+        { name: "CPU Utilization", x: new Date('2022-05-15'), y: 0 },
+      ]
+    },
+];  
+
+
+export const tick_values_7 = [
+    new Date('2022-04-01'),
+    new Date('2022-04-02'),
+    new Date('2022-04-03'),
+    new Date('2022-04-04'),
+    new Date('2022-04-05'),
+    new Date('2022-04-06'),
+    new Date('2022-04-07')
+];
+
+export const tick_values_45 = [
+    new Date('2022-04-04'),
+    new Date('2022-04-11'),
+    new Date('2022-04-18'),
+    new Date('2022-04-25'),
+    new Date('2022-05-01'),
+    new Date('2022-05-08'),
+    new Date('2022-05-15')
+];
--- src/Components/HistoricalDataChart/HistoricalDataChart.js
+++ src/Components/HistoricalDataChart/HistoricalDataChart.js
@@ -12,37 +12,17 @@ import { Dropdown, DropdownItem, DropdownToggle, Tooltip } from '@patternfly/rea
 import CaretDownIcon from '@patternfly/react-icons/dist/js/icons/caret-down-icon';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 import './HistoricalDataChart.scss';
+import { series, series_7_days, series_less_days_45, series_random_days, tick_values_45, tick_values_7 } from './Constant';
 
 
 
 export const HistoricalDataChart = () => {
 
     const [isOpen, setOpen] = useState(false);
-
-    const series = [
-        {
-          datapoints: [
-            { name: "CPU Utilization", x: "1 Apr", y: 10 },
-            { name: "CPU Utilization", x: "2 Apr", y: 20 },
-            { name: "CPU Utilization", x: "3 Apr", y: 30 },
-            { name: "CPU Utilization", x: "4 Apr", y: 50 },
-            { name: "CPU Utilization", x: "5 Apr", y: 40 },
-            { name: "CPU Utilization", x: "6 Apr", y: 30 },
-            { name: "CPU Utilization", x: "Today", y: 20 }
-          ]
-        },
-        {
-          datapoints: [
-            { name: "Memory Utilization", x: "1 Apr", y: 20 },
-            { name: "Memory Utilization", x: "2 Apr", y: 25 },
-            { name: "Memory Utilization", x: "3 Apr", y: 30 },
-            { name: "Memory Utilization", x: "4 Apr", y: 40 },
-            { name: "Memory Utilization", x: "5 Apr", y: 30 },
-            { name: "Memory Utilization", x: "6 Apr", y: 20 },
-            { name: "Memory Utilization", x: "Today", y: 10 }
-          ]
-        }
-    ];  
+    const [dateRange, setDateRange] = useState(7);
+    const [zoomedXDomain, setZoomedXDomain] = useState();
+    const [chartData, setChartData] = useState(series_7_days);
+    const [xTickValue, setXTickValues] = useState(tick_values_7)
     
     const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi");
 
@@ -50,19 +30,49 @@ export const HistoricalDataChart = () => {
         setOpen(isOpen);
     }
 
-    const onSelect = (event, value) => {
+    const onSelect = () => {
         setOpen(!isOpen);
-        console.log("checking:", event, value);
+    }
+
+    const updateChart = (dateRange) => {
+        setDateRange(dateRange);
+        if(dateRange === 7){
+            setChartData(series_7_days);
+            setXTickValues(tick_values_7);
+            setZoomedXDomain([new Date('2022-04-01'), new Date('2022-04-07')])
+        } else if(dateRange === 45){
+            setChartData(series);
+            setXTickValues(tick_values_45);
+            setZoomedXDomain([new Date('2022-04-04'), new Date('2022-05-15')])
+        }
     }
 
     const dropdownItems =[
-        <DropdownItem key="action" component="button">Last 7 Days</DropdownItem>,
-        <DropdownItem key="action" component="button">Last 45 Days</DropdownItem>
+        <DropdownItem key="action" component="button" onClick={() => updateChart(7)}>Last 7 Days</DropdownItem>,
+        <DropdownItem key="action" component="button" onClick={() => updateChart(45)}>Last 45 Days</DropdownItem>
     ]
 
+    const getEntireDomain = () => {
+        return {
+            y: [0, 100],
+            x: [1, 14]
+        };
+    }
+
+    const handleDomainChange = (domain) => {
+        console.log("Handling zoom domain:", domain);
+        //setZoomedXDomain(domain.x);
+    }
+
+    const getData = () => {
+        console.log("Getting data");
+    }
+
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
     return(
-        <div>
-            <span>
+        <div className='chartContainer'>
+            <span className='dropdownContainer'>
                 <Tooltip content={<div>Scroll or pan to zoom in</div>}>
                     <OutlinedQuestionCircleIcon size='sm' />
                 </Tooltip>
@@ -73,7 +83,7 @@ export const HistoricalDataChart = () => {
                         <DropdownToggle 
                         id='chart-date-toggle' 
                         onToggle={onToggle} toggleIndicator={CaretDownIcon} >
-                        Last 7 Days
+                        {`Last ${dateRange} Days`}
                         </DropdownToggle>
                     }
                     isOpen={isOpen}
@@ -82,57 +92,69 @@ export const HistoricalDataChart = () => {
             </span>
             
             <div style={{ height: "275px" }}>
-            <Chart
-                ariaDesc="System Utilization"
-                ariaTitle="System Utilization"
-                containerComponent={
-                    <VictoryZoomVoronoiContainer 
-                        labels={({ datum }) => datum.childName.includes('line-') ? `${datum.name}: ${datum.y}%` : null}  
-                        constrainToVisibleArea
-                    />
-                }
-                legendData={[{ name: "CPU Utilization" }, { name: "Memory Utilization" }]}
-                legendOrientation="vertical"
-                legendPosition="right"
-                height={275}
-                width={756}
-                maxDomain={{ y: 100 }}
-                minDomain={{ y: 0 }}
-                padding={{
-                    bottom: 50,
-                    left: 50,
-                    right: 200, // Adjusted to accommodate legend
-                    top: 50
-                }}
-                themeColor={ChartThemeColor.blue}>
-                <ChartAxis />
-                <ChartAxis 
-                    dependentAxis showGrid 
-                    tickValues={[0, 20, 40, 60, 80, 100]}
-                    tickFormat={(t) => `${t}%`} />
-                <ChartGroup>
-                {series.map((s, idx) => {
-                    return (
-                    <ChartScatter
-                        data={s.datapoints}
-                        key={"scatter-" + idx}
-                        name={"scatter-" + idx}
-                    />
-                    );
-                })}
-                </ChartGroup>
-                <ChartGroup>
-                {series.map((s, idx) => {
-                    return (
-                    <ChartLine
-                        key={"line-" + idx}
-                        name={"line-" + idx}
-                        data={s.datapoints}
-                    />
-                    );
-                })}
-                </ChartGroup>
-            </Chart>
+                <Chart
+                    scale={{x: "time"}}
+                    ariaDesc="System Utilization"
+                    ariaTitle="System Utilization"
+                    containerComponent={
+                        <VictoryZoomVoronoiContainer 
+                            labels={({ datum }) => datum.childName.includes('line-') ? `${datum.name}: ${datum.y}%` : null}  
+                            constrainToVisibleArea
+                            zoomDimension="x"
+                            onZoomDomainChange={(domain) => handleDomainChange(domain)}
+                            zoomDomain={{x: zoomedXDomain}}
+                        />
+                    }
+                    legendData={[{ name: "CPU Utilization" }, { name: "Memory Utilization" }]}
+                    legendOrientation="vertical"
+                    legendPosition="right"
+                    height={275}
+                    width={756}
+                    maxDomain={{ y: 100 }}
+                    minDomain={{ y: 0 }}
+                    padding={{
+                        bottom: 50,
+                        left: 50,
+                        right: 200, // Adjusted to accommodate legend
+                        top: 50
+                    }}
+                    themeColor={ChartThemeColor.blue}>
+
+                    <ChartAxis 
+                        tickValues={xTickValue}
+                        tickFormat={(x) => `${new Date(x).getDate()} ${months[new Date(x).getMonth()]}`}
+                        />
+                    <ChartAxis 
+                        dependentAxis showGrid 
+                        tickValues={[0, 20, 40, 60, 80, 100]}
+                        tickFormat={(t) => `${t}%`} />
+
+
+                    <ChartGroup>
+                        {chartData.map((s, idx) => {
+                            return (
+                            <ChartScatter
+                                data={s.datapoints}
+                                key={"scatter-" + idx}
+                                name={"scatter-" + idx}
+                            />
+                            );
+                        })}
+                    </ChartGroup>
+
+                    <ChartGroup>
+                        {chartData.map((s, idx) => {
+                            return (
+                            <ChartLine
+                                key={"line-" + idx}
+                                name={"line-" + idx}
+                                data={s.datapoints}
+                            />
+                            );
+                        })}
+                    </ChartGroup>
+
+                </Chart>
             </div>
         </div>
     );
