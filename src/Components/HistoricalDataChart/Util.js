3ecbe275f12180819bbda38014addf4d6124c6a3
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-04-27 13:58:45 +0530

    fixed lint errors

--- src/Components/HistoricalDataChart/Util.js
+++ src/Components/HistoricalDataChart/Util.js
@@ -12,21 +12,21 @@ export const formatHistoricalData = (responseData, dateRange) =>{
     };
 
     let mapResponseData = new Map();
-    
-    for (let index=0; index<responseData.length; index++) {
-        mapResponseData.set(new Date(responseData[index].report_date).toDateString() ,responseData[index]);
+
+    for (let index = 0; index < responseData.length; index++) {
+        mapResponseData.set(new Date(responseData[index].report_date).toDateString(), responseData[index]);
     }
 
     for (let index = (dateRange - 1); index >= 0; index--) {
         let dateToCheck = new Date();
         dateToCheck =   new Date(dateToCheck.setDate(today.getDate() - index)).toDateString();
-        if (mapResponseData.has(dateToCheck)){
+        if (mapResponseData.has(dateToCheck)) {
             cpu.datapoints.push({
                 name: 'CPU Utilization',
                 x: new Date(dateToCheck),
                 y: mapResponseData.get(dateToCheck).cpu
             });
-                        
+
             memory.datapoints.push({
                 name: 'Memory Utilization',
                 x: new Date(dateToCheck),
@@ -39,7 +39,7 @@ export const formatHistoricalData = (responseData, dateRange) =>{
                 x: new Date(dateToCheck),
                 y: null
             });
-        
+
             memory.datapoints.push({
                 name: 'Memory Utilization',
                 x: new Date(dateToCheck),
