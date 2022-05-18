e23f327403e1a4d82f1ec54f2d698d8f05b6957e
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-05-19 00:21:45 +0530

    RHIROS-600  Executive report (#111)
    * RHIROS-600  added UI components for creating executive report
    
    * executive report API call and code refactoring
    
    * get and format data from executive report api
    
    * minor fixes in registerd systems section
    
    * removed undersized and oversized from IO conditions table
    
    * minor text update

--- src/Components/Reports/Util.js
+++ src/Components/Reports/Util.js
@@ -70,3 +70,68 @@ export const getSystemsReportFileName = () =>  {
 
     return reportFileName;
 };
+
+export const formatExecutiveReportData = (data) => {
+
+    const { systems_per_state: systemsPerState, conditions } = data;
+
+    const stateChartData = [];
+    const stateTableData = [['# of systems']];
+
+    const conditionsChartData = [];
+    const conditionsTableData = [['# of occurrences']];
+
+    const condtionsInfo = {
+        io: {
+            name: 'Disk IO',
+            occurances: []
+        },
+        memory: {
+            name: 'RAM',
+            occurances: []
+        },
+        cpu: {
+            name: 'CPU',
+            occurances: []
+        }
+    };
+
+    const stateNameMapping = {
+        idling: 'Idling',
+        oversized: 'Oversized',
+        under_pressure: 'Under pressure', /* eslint-disable-line camelcase */
+        undersized: 'Undersized',
+        waiting_for_data: 'Waiting for data', /* eslint-disable-line camelcase */
+        optimized: 'Optimized'
+    };
+
+    Object.keys(systemsPerState).map((state) => {
+        const stateName = stateNameMapping[state];
+        const percentage = Math.floor(systemsPerState[state]?.percentage);
+        const count = systemsPerState[state]?.count;
+
+        stateChartData.push({ x: stateName, y: percentage });
+        stateTableData.push([`${count} (${percentage}% of total)`]);
+    });
+
+    Object.keys(conditions).map((condition) => {
+        const conditionName = condtionsInfo[condition].name;
+        const percentage = Math.floor(conditions[condition]?.percentage);
+        const count = conditions[condition]?.count;
+
+        conditionsChartData.push({ x: conditionName, y: percentage });
+        conditionsTableData.push([`${count}`]);
+
+        const underPressureValue = (conditions[condition].under_pressure !== null) ? conditions[condition].under_pressure : 0;
+        const undersizedValue = (conditions[condition].undersized !== null) ? conditions[condition].undersized : 0;
+        const oversizedValue = (conditions[condition].oversized !== null) ? conditions[condition].oversized : 0;
+
+        condtionsInfo[condition].occurances.push(['Under pressure', `${underPressureValue}`]);
+        if (undersizedValue !== -1 && oversizedValue !== -1) {
+            condtionsInfo[condition].occurances.push(['Undersized', `${undersizedValue}`]);
+            condtionsInfo[condition].occurances.push(['Oversized', `${oversizedValue}`]);
+        }
+    });
+
+    return { stateChartData, stateTableData, conditionsChartData, conditionsTableData, condtionsInfo };
+};
