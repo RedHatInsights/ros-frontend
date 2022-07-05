80967781ffbe453872c7116ba45ef9dcee37b495
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-07-05 18:16:38 +0530

    added date info in chart tooltip

--- src/Components/HistoricalDataChart/HistoricalDataChart.js
+++ src/Components/HistoricalDataChart/HistoricalDataChart.js
@@ -123,7 +123,10 @@ export const HistoricalDataChart = ({ inventoryId }) => {
                         containerComponent={
                             <VictoryZoomVoronoiContainer
                                 labels={({ datum }) => {
-                                    return datum.childName.includes('scatter-') && datum.y !== null ? `${datum.name}: ${datum.y}%` : null;}
+                                    console.log(datum);
+                                    const isToday = new Date().toDateString() === new Date(datum.x).toDateString();
+                                    const xAxisDate = isToday ? 'Today' : `${new Date(datum.x).getDate()} ${MONTHS[new Date(datum.x).getMonth()]}`;
+                                    return datum.childName.includes('scatter-') && datum.y !== null ? `${xAxisDate}\n${datum.name}: ${datum.y}%` : null;}
                                 }
                                 constrainToVisibleArea
                                 voronoiDimension="x"
