1c6151c0eefc7c5274873a4961a537c3a26d4eca
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-07-18 17:28:11 +0530

    removed console statements

--- src/Components/HistoricalDataChart/HistoricalDataChart.js
+++ src/Components/HistoricalDataChart/HistoricalDataChart.js
@@ -123,7 +123,6 @@ export const HistoricalDataChart = ({ inventoryId }) => {
                         containerComponent={
                             <VictoryZoomVoronoiContainer
                                 labels={({ datum }) => {
-                                    console.log('checking data:', datum);
                                     const isToday = new Date().toDateString() === new Date(datum.x).toDateString();
                                     const xAxisDate = isToday ? 'Today' : `${new Date(datum.x).getDate()} ${MONTHS[new Date(datum.x).getMonth()]}`;
                                     return datum.childName.includes('scatter-')
