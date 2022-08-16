2f3ff66c43a7246ee9af69c7415388ace13fc006
kgaikwad <kavitagaikwad103@gmail.com>
2022-08-16 19:15:16 +0530

    Fixes #RHIROS-761 - fix disorted detail page for culled systems

--- src/Components/HistoricalDataChart/HistoricalDataChart.js
+++ src/Components/HistoricalDataChart/HistoricalDataChart.js
@@ -10,7 +10,7 @@ import {
 } from '@patternfly/react-charts';
 import {
     Bullseye, Dropdown, DropdownItem, DropdownToggle,
-    EmptyState, EmptyStateBody, EmptyStateVariant, Spinner, Title, Tooltip
+    EmptyState, EmptyStateBody, EmptyStateVariant, Spinner, Title, Tooltip, Flex, FlexItem
 } from '@patternfly/react-core';
 import CaretDownIcon from '@patternfly/react-icons/dist/js/icons/caret-down-icon';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
@@ -76,43 +76,47 @@ export const HistoricalDataChart = ({ inventoryId }) => {
     };
 
     const displayError = () => {
-        return <EmptyState className='loadingErrorContainer' variant={EmptyStateVariant.small}>
-            <Title headingLevel="h2" size="lg">
-                Something went wrong
-            </Title>
-            <EmptyStateBody>
-                There was a problem while requesting historical data. Please try again later.
-            </EmptyStateBody>
-        </EmptyState>;
+        return  <FlexItem  alignSelf={{ default: 'alignSelfBaseline' }}>
+            <EmptyState className='loadingErrorContainer' variant={EmptyStateVariant.small}>
+                <Title headingLevel="h2" size="lg">
+                    Something went wrong
+                </Title>
+                <EmptyStateBody>
+                    There was a problem while requesting historical data. Please try again later.
+                </EmptyStateBody>
+            </EmptyState>
+        </FlexItem>;
     };
 
     const displayChart = () => {
-
         return  chartData.length === 0 ?
-            <Bullseye className='loadingErrorContainer'>
-                <Spinner size="xl" aria-labelledby="loading-historical-chart"/>
-            </Bullseye> :
-            <Fragment>
-                <span className='dropdownContainer'>
-                    <Tooltip content={<div>Scroll and pan to zoom and move</div>}>
-                        <OutlinedQuestionCircleIcon size='sm' />
-                    </Tooltip>
-                    <Dropdown
-                        className='dateDropdown'
-                        toggle={
-                            <DropdownToggle
-                                id='chart-date-toggle'
-                                onToggle={onToggle}
-                                toggleIndicator={CaretDownIcon} >
-                                {`Last ${dateRange === DATE_RANGE_7_DAYS ? DATE_RANGE_7_DAYS : RANGE_DROPDOWN_45_DAYS} Days`}
-                            </DropdownToggle>
-                        }
-                        isOpen={isOpen}
-                        dropdownItems={dropdownItems}
-                    />
-                </span>
-
-                <div style={{ height: '275px' }}>
+            <FlexItem  alignSelf={{ default: 'alignSelfBaseline' }}>
+                <Bullseye className='loadingErrorContainer'>
+                    <Spinner size="xl" aria-labelledby="loading-historical-chart"/>
+                </Bullseye>
+            </FlexItem> :
+            <Flex direction={{ default: 'column' }} alignSelf={{ default: 'alignSelfBaseline' }} align={{ default: 'alignRight' }}>
+                <FlexItem align={{ default: 'alignRight' }}>
+                    <span className='dropdownContainer'>
+                        <Tooltip content={<div>Scroll and pan to zoom and move</div>}>
+                            <OutlinedQuestionCircleIcon size='sm' />
+                        </Tooltip>
+                        <Dropdown
+                            className='dateDropdown'
+                            toggle={
+                                <DropdownToggle
+                                    id='chart-date-toggle'
+                                    onToggle={onToggle}
+                                    toggleIndicator={CaretDownIcon} >
+                                    {`Last ${dateRange === DATE_RANGE_7_DAYS ? DATE_RANGE_7_DAYS : RANGE_DROPDOWN_45_DAYS} Days`}
+                                </DropdownToggle>
+                            }
+                            isOpen={isOpen}
+                            dropdownItems={dropdownItems}
+                        />
+                    </span>
+                </FlexItem>
+                <FlexItem align={{ default: 'alignRight' }}>
                     <Chart
                         domain={getEntireDomain()}
                         scale={{ x: 'time', y: 'linear' }}
@@ -183,18 +187,12 @@ export const HistoricalDataChart = ({ inventoryId }) => {
                         </ChartGroup>
 
                     </Chart>
-                </div>
-            </Fragment>;
-
+                </FlexItem>
+            </Flex>;
     };
 
     return (
-        <div className='chartContainer'>
-            {
-                showError ?  displayError() : displayChart()
-
-            }
-        </div>
+        showError ?  displayError() : displayChart()
     );
 
 };
