061356a2a9d43cde3a03a282cce8f00825a2aed7
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-09-30 14:32:48 +0530

    added error component for download button

--- src/Components/Reports/ExecutivePDFReport.js
+++ src/Components/Reports/ExecutivePDFReport.js
@@ -66,6 +66,7 @@ export const DownloadExecutivePDFReport = ({ isDisabled }) => {
                     isAriaDisabled: isDisabled,
                     ...(loading ? { isDisabled: true } : null)
                 }}
+                ErrorComponent=<div>Error while loading executive report</div>
             />
         </Fragment>
     );
