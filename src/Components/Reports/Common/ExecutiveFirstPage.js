1b203233f328a63585cf5cfdcd5df3f351bc3455
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-12-02 22:37:55 +0530

    minor text updates

--- src/Components/Reports/Common/ExecutiveFirstPage.js
+++ src/Components/Reports/Common/ExecutiveFirstPage.js
@@ -112,7 +112,7 @@ export const ExecutiveFirstPage = ({ data }) => {
             nonPSICount > 0
              && <Text>{
                  /* eslint-disable-next-line max-len */
-                 `${psiEnabledCount} systems out of a total of ${totalCount} systems have Kernel Pressure Stall Information enabled. You could get better suggestions for ${nonPSICount} systems if you enabled Pressure Stall Information. Check the documentation to learn how.`
+                 `${psiEnabledCount} systems out of a total of ${totalCount} systems have Kernel Pressure Stall Information enabled. You could get better suggestions for ${nonPSICount} systems if you enabled Pressure Stall Information. Check the documentation on how to enable PSI on versions RHEL 8 and newer.`
              }</Text>
 
         }
