3b16a801108fb46d28d1d092e43b1c8159935a4d
Preeti Wadhwani <pwadhwani2592@gmail.com>
2022-12-29 15:03:02 +0530

    Fixes #RHIROS-526 - update node version to 16  (#150)
    * RHIROS-526 updated node version to 16 in travis.yml
    
    * updated node version to 16 in pr_check and build_deploy files

--- pr_check.sh
+++ pr_check.sh
@@ -9,7 +9,7 @@ export COMPONENT="ros"
 export IMAGE="quay.io/cloudservices/ros-frontend"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
 export APP_ROOT=$(pwd)
-export NODE_BUILD_VERSION=14
+export NODE_BUILD_VERSION=16
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 
 # --------------------------------------------
