5d980632f0a35e9aa545c774c88e159f692050f1
Ryan Deussing <ryandeussing@gmail.com>
2023-02-21 13:04:22 -0500

    update docs versions for Insights, Console, Cost & Subscriptions

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { nowrap } from '@patternfly/react-table';
 import { Tooltip } from '@patternfly/react-core';
-import { displayState, recommendations, scoreProgress, systemName } from './store/entitiesReducer';
-import { diskUsageData, displayLastReported, displayOS } from './Components/RosTable/RenderColumn';
+import {
+  displayState,
+  recommendations,
+  scoreProgress,
+  systemName,
+} from './store/entitiesReducer';
+import {
+  diskUsageData,
+  displayLastReported,
+  displayOS,
+} from './Components/RosTable/RenderColumn';
 
 // API
 export const ROS_API_ROOT = '/api/ros/v1';
@@ -22,42 +31,43 @@ export const NO_DATA_VALUE = 'N/A';
 
 // Getting started URL
 // eslint-disable-next-line max-len
-export const GETTING_STARTED_URL = 'https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/con-ros-overview_ros-overview';
+export const GETTING_STARTED_URL =
+  'https://access.redhat.com/documentation/en-us/red_hat_insights/2023/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/con-ros-overview_ros-overview';
 
 // Custom Filters
 export const CUSTOM_FILTERS = {
-    state: {
-        label: 'State',
-        type: 'checkbox',
-        filterValues: {
-            items: [
-                {
-                    label: 'Idling',
-                    value: 'Idling'
-                },
-                {
-                    label: 'Optimized',
-                    value: 'Optimized'
-                },
-                {
-                    label: 'Oversized',
-                    value: 'Oversized'
-                },
-                {
-                    label: 'Under pressure',
-                    value: 'Under pressure'
-                },
-                {
-                    label: 'Undersized',
-                    value: 'Undersized'
-                },
-                {
-                    label: 'Waiting for data',
-                    value: 'Waiting for data'
-                }
-            ]
-        }
-    }
+  state: {
+    label: 'State',
+    type: 'checkbox',
+    filterValues: {
+      items: [
+        {
+          label: 'Idling',
+          value: 'Idling',
+        },
+        {
+          label: 'Optimized',
+          value: 'Optimized',
+        },
+        {
+          label: 'Oversized',
+          value: 'Oversized',
+        },
+        {
+          label: 'Under pressure',
+          value: 'Under pressure',
+        },
+        {
+          label: 'Undersized',
+          value: 'Undersized',
+        },
+        {
+          label: 'Waiting for data',
+          value: 'Waiting for data',
+        },
+      ],
+    },
+  },
 };
 
 // Query params for state filters
@@ -67,94 +77,107 @@ export const WITH_WAITING_FOR_DATA_PARAM = 'with_waiting_for_data';
 // Systems table columns
 
 export const SYSTEM_TABLE_COLUMNS = [
-    {
-        key: 'display_name',
-        title: 'Name',
-        modalTitle: 'Name',
-        renderFunc: (data, id, item) => systemName(data, id, item),
-        isChecked: true,
-        isDisabled: true,
-        isShownByDefault: true
-    },
-    {
-        key: 'os',
-        title: (
-            <Tooltip content={<span>Operating system</span>}>
-                <span>OS</span>
-            </Tooltip>
-        ),
-        modalTitle: 'Operating system',
-        dataLabel: 'Operating system',
-        renderFunc: (data) => displayOS(data),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true
-    },
-    {
-        key: 'performance_utilization.cpu',
-        title: 'CPU utilization',
-        modalTitle: 'CPU utilization',
-        renderFunc: (data, id, item) => scoreProgress(data, id, item),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true
-    },
-    {
-        key: 'performance_utilization.memory',
-        title: 'Memory utilization',
-        modalTitle: 'Memory utilization',
-        renderFunc: (data, id, item) => scoreProgress(data, id, item),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true
-    },
-    {
-        key: 'performance_utilization.max_io',
-        title: (
-            <Tooltip content={<span>IOPS</span>}>
-                <span>I/O utilization</span>
-            </Tooltip>
-        ),
-        modalTitle: 'I/O utilization',
-        dataLabel: 'I/O utilization',
-        renderFunc: (data, id, item) => diskUsageData(data, id, item),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true
-    },
-    {
-        key: 'number_of_suggestions',
-        title: 'Suggestions',
-        modalTitle: 'Suggestions',
-        renderFunc: (data, id, item) => recommendations(data, id, item),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true
-    },
-    {
-        key: 'state',
-        title: 'State',
-        modalTitle: 'State',
-        renderFunc: (data) => displayState(data),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true
-    },
-    {
-        key: 'report_date',
-        title: 'Last reported',
-        modalTitle: 'Last reported',
-        renderFunc: (data) => displayLastReported(data),
-        isChecked: true,
-        isDisabled: false,
-        isShownByDefault: true,
-        transforms: [nowrap],
-        cellTransforms: [nowrap]
-    }
+  {
+    key: 'display_name',
+    title: 'Name',
+    modalTitle: 'Name',
+    renderFunc: (data, id, item) => systemName(data, id, item),
+    isChecked: true,
+    isDisabled: true,
+    isShownByDefault: true,
+  },
+  {
+    key: 'os',
+    title: (
+      <Tooltip content={<span>Operating system</span>}>
+        <span>OS</span>
+      </Tooltip>
+    ),
+    modalTitle: 'Operating system',
+    dataLabel: 'Operating system',
+    renderFunc: (data) => displayOS(data),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+  },
+  {
+    key: 'performance_utilization.cpu',
+    title: 'CPU utilization',
+    modalTitle: 'CPU utilization',
+    renderFunc: (data, id, item) => scoreProgress(data, id, item),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+  },
+  {
+    key: 'performance_utilization.memory',
+    title: 'Memory utilization',
+    modalTitle: 'Memory utilization',
+    renderFunc: (data, id, item) => scoreProgress(data, id, item),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+  },
+  {
+    key: 'performance_utilization.max_io',
+    title: (
+      <Tooltip content={<span>IOPS</span>}>
+        <span>I/O utilization</span>
+      </Tooltip>
+    ),
+    modalTitle: 'I/O utilization',
+    dataLabel: 'I/O utilization',
+    renderFunc: (data, id, item) => diskUsageData(data, id, item),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+  },
+  {
+    key: 'number_of_suggestions',
+    title: 'Suggestions',
+    modalTitle: 'Suggestions',
+    renderFunc: (data, id, item) => recommendations(data, id, item),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+  },
+  {
+    key: 'state',
+    title: 'State',
+    modalTitle: 'State',
+    renderFunc: (data) => displayState(data),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+  },
+  {
+    key: 'report_date',
+    title: 'Last reported',
+    modalTitle: 'Last reported',
+    renderFunc: (data) => displayLastReported(data),
+    isChecked: true,
+    isDisabled: false,
+    isShownByDefault: true,
+    transforms: [nowrap],
+    cellTransforms: [nowrap],
+  },
 ];
 
 // Historical Chart
-export const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+export const MONTHS = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 
 export const DATE_RANGE_7_DAYS = 7;
 export const DATE_RANGE_49_DAYS = 49;
