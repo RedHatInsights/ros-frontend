e6778492eafe3f6a979b7c870ceeac5294bab094
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2023-04-20 13:47:08 +0530

    RHIROS-1064 show psi-related info under ROS details page (#192)
    * RHIROS-1064 show psi-related info under ROS details page
    
    * add unit tests
    
    * replace anchor tag with text component

--- src/Components/SystemDetail/RecommendationsTable.test.js
+++ src/Components/SystemDetail/RecommendationsTable.test.js
@@ -6,7 +6,7 @@ import renderer from 'react-test-renderer';
 describe('RecommendationsTable component', () => {
     afterEach(cleanup);
 
-    it ('matches snapshot', ()=>{
+    it ('matches snapshot when psi is disabled', ()=>{
         const propValues = {
             recommendations: [{
                 ruleId: 'cloud_instance_ros_evaluation|CONSUMPTION_MODEL',
@@ -14,7 +14,34 @@ describe('RecommendationsTable component', () => {
                 reason: 'This may be caused due to inappropriate consumption model.',
                 resolution: 'Please verify this instance.',
                 condition: ''
-            }]
+            }],
+            psiEnabled: false
+        };
+
+        const component = renderer.create(<RecommendationsTable { ...propValues }/>);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it ('matches snapshot when psi is enabled', ()=>{
+        const propValues = {
+            recommendations: [{
+                ruleId: 'cloud_instance_ros_evaluation|CONSUMPTION_MODEL',
+                description: 'This is test rule description.',
+                reason: 'This may be caused due to inappropriate consumption model.',
+                resolution: 'Please verify this instance.',
+                condition: ''
+            }],
+            psiEnabled: true
+        };
+
+        const component = renderer.create(<RecommendationsTable { ...propValues }/>);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it ('matches snapshot where there is no suggestions', ()=>{
+        const propValues = {
+            recommendations: [],
+            psiEnabled: false
         };
 
         const component = renderer.create(<RecommendationsTable { ...propValues }/>);
