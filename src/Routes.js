4c8d137c8ae8c108ebcb5839a56f81d107d4da8c
Adonis Puente <adonis.puente@gmail.com>
2023-05-02 13:55:28 -0400

    feat(RHIN-830): Add ZeroState to ROS

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,52 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { Bullseye, Spinner } from '@patternfly/react-core';
+import AsynComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
+import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
+import axios from 'axios';
 
 const RosPage = lazy(() => import(/* webpackChunkName: "RosPage" */ './Routes/RosPage/RosPage'));
 const RosSystemDetail = lazy(() => import(/* webpackChunkName: "RosSystemDetail" */ './Routes/RosSystemDetail/RosSystemDetail'));
 
-export const Routes = () => (
-    <Suspense fallback={<Bullseye>
-        <Spinner />
-    </Bullseye>}>
-        <Switch>
-            <Route exact path='/' component={RosPage} />
-            <Route path='/:inventoryId' component={RosSystemDetail} />
-            <Route>
-                <Redirect to='/ros' />
-            </Route>
-        </Switch>
-    </Suspense>
-);
+const INVENTORY_TOTAL_FETCH_URL = '/api/inventory/v1/hosts';
+
+export const Routes = () => {
+    const [hasSystems, setHasSystems] = useState(false);
+    useEffect(() => {
+        try {
+            axios
+            .get(`${INVENTORY_TOTAL_FETCH_URL}?page=1&per_page=1`)
+            .then(({ data }) => {
+                setHasSystems(data.total > 0);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }, [setHasSystems]);
+
+    return (
+        <Suspense fallback={<Bullseye>
+            <Spinner />
+        </Bullseye>}>
+            {!hasSystems ?
+                <AsynComponent
+                    appName="dashboard"
+                    module="./AppZeroState"
+                    scope="dashboard"
+                    ErrorComponent={<ErrorState />}
+                    app="Resource_Optimization"
+                /> :
+                <Switch>
+                    <Route exact path='/' component={RosPage} />
+                    <Route path='/:inventoryId' component={RosSystemDetail} />
+                    <Route>
+                        <Redirect to='/ros' />
+                    </Route>
+                </Switch>
+            }
+        </Suspense>);
+};
 
 Routes.propTypes = {
     childProps: PropTypes.shape({
