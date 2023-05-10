616c4e09d5339c65ac24690679ae1668c7988236
Preeti Wadhwani <pwadhwani2592@gmail.com>
2023-05-10 15:31:07 +0530

    reverted zero state changes

--- src/Components/ServiceNotConfigured/ServiceNotConfigured.js
+++ src/Components/ServiceNotConfigured/ServiceNotConfigured.js
@@ -1,19 +1,21 @@
 import React, { Suspense } from 'react';
 import { Bullseye, Spinner } from '@patternfly/react-core';
-import AsynComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
-import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
+// import AsynComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
+// import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
 
 export const ServiceNotConfigured = () => (
     <Suspense fallback={<Bullseye>
         <Spinner />
     </Bullseye>}>
-        <AsynComponent
+        {/* <AsynComponent
             appId="ros_zero_state"
             appName="dashboard"
             module="./AppZeroState"
             scope="dashboard"
             ErrorComponent={<ErrorState />}
             app="Resource_Optimization"
-        />
+        /> */
+            <div>ROS is not configured</div>
+        }
     </Suspense>
 );
