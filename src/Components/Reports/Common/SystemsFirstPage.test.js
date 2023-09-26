d2a31d18965b88ab8e6a6246ff8d1370793767e1
Preeti Wadhwani <pwadhwani2592@gmail.com>
2023-09-26 19:41:01 +0530

    added tests and style fix

--- src/Components/Reports/Common/SystemsFirstPage.test.js
+++ src/Components/Reports/Common/SystemsFirstPage.test.js
@@ -8,9 +8,9 @@ describe('Systems First Page Component', () => {
 
         const firstPageProps = {
             data: [
-                ['iqe-ros-bb519f81', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Undersized'],
-                ['iqe-ros-bb519f81', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Under pressure'],
-                ['iqe-ros-bb519f81', 'RHEL 8.4', 'N/A', 'N/A', 'N/A', '2', 'Waiting for data']
+                ['iqe-ros-bb519f81', 'N/A', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Undersized'],
+                ['iqe-ros-bb519f81', 'team-ros', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Under pressure'],
+                ['iqe-ros-bb519f81', 'N/A', 'RHEL 8.4', 'N/A', 'N/A', 'N/A', '2', 'Waiting for data']
             ],
             totalSystems: 3,
             filterText: `Filters applied\nState: Undersized,Under pressure,Waiting for data\nName: iqe`
@@ -26,10 +26,10 @@ describe('Systems First Page Component', () => {
 
         const firstPageProps = {
             data: [
-                ['iqe-ros-bb519f81', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Undersized']
+                ['iqe-ros-bb519f81', 'team-ros', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Undersized']
             ],
             totalSystems: 1,
-            filterText: `Filters applied\nState: Undersized\nName: iqe`
+            filterText: `Filters applied\nState: Undersized\nName: iqe\nGroups:team-ros`
         };
 
         const component = renderer.create(<SystemsFirstPage {...firstPageProps} />).toJSON();
