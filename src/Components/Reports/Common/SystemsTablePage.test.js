6a64c0b76f2850996f75e627c43da7a661de3ce2
Preeti Wadhwani <pwadhwani2592@gmail.com>
2024-05-15 14:16:21 +0530

    added unit tests

--- src/Components/Reports/Common/SystemsTablePage.test.js
+++ src/Components/Reports/Common/SystemsTablePage.test.js
@@ -1,3 +1,26 @@
-import { test } from '@jest/globals';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { SystemsTablePage } from './SystemsTablePage';
 
-test.todo('Systems Table Page Component');
+describe('Systems Table Page Component', () => {
+
+    it('should matches snapshot for systems table page and renders correctly', () => {
+
+        // render & query
+        const tableProps = {
+            data: [
+                ['iqe-ros-bb519f81', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Undersized', '2022-03-29'],
+                ['iqe-ros-bb519f81', 'RHEL 8.4', '90%', '80%', '0.314', '2', 'Under pressure', '2022-03-30'],
+                ['iqe-ros-bb519f81', 'RHEL 8.4', 'N/A', 'N/A', 'N/A', '2', 'Waiting for data', '2022-03-31']
+            ],
+            page: 1
+        };
+
+        const { asFragment } = render(<SystemsTablePage {...tableProps} />);
+
+        // assert
+        expect(asFragment()).toMatchSnapshot();
+
+    });
+
+});
--- src/Components/SystemDetail/RecommendationsTable.test.js
+++ src/Components/SystemDetail/RecommendationsTable.test.js
@@ -1,3 +1,65 @@
-import { test } from '@jest/globals';
+// import { test } from '@jest/globals';
 
-test.todo('RecommendationsTable component tests');
+// test.todo('RecommendationsTable component tests');
+
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import RecommendationsTable from './RecommendationsTable';
+
+describe('RecommendationsTable component', () => {
+
+    it ('matches snapshot when psi is disabled', ()=>{
+
+        // render & query
+        const propValues = {
+            /* eslint-disable camelcase */
+            recommendations: [{
+                rule_id: 'cloud_instance_ros_evaluation|CONSUMPTION_MODEL',
+                description: 'This is test rule description.',
+                reason: 'This may be caused due to inappropriate consumption model.',
+                resolution: 'Please verify this instance.',
+                condition: '',
+                psi_enabled: false
+            }]
+        };
+        const { asFragment } = render(<RecommendationsTable { ...propValues }/>);
+
+        // assert
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it ('matches snapshot when psi is enabled', ()=>{
+
+        // render & query
+        const propValues = {
+            /* eslint-disable camelcase */
+            recommendations: [{
+                rule_id: 'cloud_instance_ros_evaluation|CONSUMPTION_MODEL',
+                description: 'This is test rule description.',
+                reason: 'This may be caused due to inappropriate consumption model.',
+                resolution: 'Please verify this instance.',
+                condition: '',
+                psi_enabled: true
+            }]
+        };
+        const { asFragment } = render(<RecommendationsTable { ...propValues }/>);
+
+        // assert
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it ('matches snapshot where there is no suggestions', ()=>{
+
+        // render & query
+        const propValues = {
+            /* eslint-disable camelcase */
+            recommendations: [{
+                psi_enabled: false
+            }]
+        };
+        const { asFragment } = render(<RecommendationsTable { ...propValues }/>);
+
+        //assert
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
