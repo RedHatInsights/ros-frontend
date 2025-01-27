7aad26158d5d7ac028620e4cf2c16e9382fe72d5
Preeti Wadhwani <preeti.dev.git@gmail.com>
2025-01-27 15:20:58 +0530

    replaces <a> with <Text> component

--- src/Components/RosPopover/RosPopover.js
+++ src/Components/RosPopover/RosPopover.js
@@ -26,7 +26,7 @@ export const RosPopover = () => {
                         <Text component={TextVariants.p}>
                         The resource optimization service enables RHEL customers to assess and monitor their public cloud usage and optimization.
                         </Text>
-                        <a
+                        <Text component={TextVariants.a}
                             href={
                                 // eslint-disable-next-line max-len
                                 'https://docs.redhat.com/en/documentation/red_hat_insights/1-latest/html/assessing_and_monitoring_rhel_resource_optimization_with_insights_for_red_hat_enterprise_linux/index'
@@ -36,7 +36,7 @@ export const RosPopover = () => {
                         >
                             Assessing and Monitoring RHEL Resource Optimization with Insights for Red Hat Enterprise Linux
                             <ExternalLinkAltIcon className="pf-v5-u-ml-xs" />
-                        </a>
+                        </Text>
                     </Flex>
                 </TextContent>
             }
